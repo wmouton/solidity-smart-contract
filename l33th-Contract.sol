cc4ed0a4bf36b68b194893d042c7e79b9a4a6d22
l33th <l33th@protonmail.ch>
2022-01-21 07:00:35 +0200

    final contract

--- l33th-Contract.sol
+++ l33th-Contract.sol
@@ -1,19 +1,24 @@
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.9;
 
 contract LToken {
     address public minter;
-    mapping(address => uint) public balances;
+    mapping(address => uint256) public balances;
 
-    event Sent(address from, address to, uint amount);
+    event Sent(address from, address to, uint256 amount);
 
     constructor() {
         minter = msg.sender;
     }
 
-    function mint(address receiver, uint amount) public {
+    function mint(address receiver, uint256 amount) public {
         require(msg.sender == minter);
         balances[receiver] += amount;
     }
 
-    function send(address receiver, uint amount) public {}
+    function send(address receiver, uint256 amount) public {
+        require(amount <= balances[msg.sender], "Insufficient Balance");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
 }
